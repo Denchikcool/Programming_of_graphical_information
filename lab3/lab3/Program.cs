17bf27537260abf62cf9d0621ba4437c7160acd6
Denchikcool <kuzmenok.den@yandex.ru>
2025-02-09 19:43:54 +0700

    Поворот изображения на 90 градусов

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Bitmap image = new Bitmap("image.bmp"))
+            {
+                int width = image.Width;
+                int height = image.Height;
+                Bitmap rotatedImage = new Bitmap(height, width, PixelFormat.Format32bppArgb);
+
+                for(int i = 0; i < width; i++)
+                {
+                    for(int j = 0; j < height; j++)
+                    {
+                        Color pixel = image.GetPixel(i, j);
+                        rotatedImage.SetPixel(height - 1 - j, i, pixel);
+                    }
+                }
+
+                rotatedImage.Save("rotatedImage.bmp", ImageFormat.Bmp);
+            }
+        }
+    }
+}
